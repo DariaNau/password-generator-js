c70e3bdb97ad79060bb40746bd9d962362dc27c9
Daria Naumova <dariagnaumova@gmail.com>
2019-12-19 15:50:17 -0500

    variables added, welcome alert created

---
+++ script.js
@@ -0,0 +1,16 @@
+// Set the variables
+  
+   var abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   var abcUpperArr = abcUpper.split("");
+   var abcLower = "abcdefghijklmnopqrstuvwxyz";
+   var abcLowerArr = abcLower.split("");
+   var num = "0123456789";
+   var numArr = num.split("");
+   var sym = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
+   var symArr = sym.split("");
+
+//  Prompt instruction for the user to begin
+
+window.onload = alert("Welcome! Please click 'Generate password' to start!");
+
+
